86f9185a70780d0ccba00207c4c20e85ee8b9286
Kacper Plusa <plusakacper@gmail.com>
2023-03-20 19:30:10 +0100

    initial commit

---
+++ src/main/java/com/kacperp/mobilehub/MobilehubApplication.java
@@ -0,0 +1,13 @@
+package com.kacperp.mobilehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobilehubApplication.class, args);
+    }
+
+}
